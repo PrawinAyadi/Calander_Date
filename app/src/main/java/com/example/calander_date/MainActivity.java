8cc47919082140b0e7170d68542182a65f5fa340
PrawinAyadi <49094521+PrawinAyadi@users.noreply.github.com>
2019-10-26 19:37:46 +0545

    uupdate time

--- app/src/main/java/com/example/calander_date/MainActivity.java
+++ app/src/main/java/com/example/calander_date/MainActivity.java
@@ -3,28 +3,38 @@ package com.example.calander_date;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.TextView;
-
+import android.widget.TimePicker;
 
 
 import java.util.Calendar;
 import java.util.Date;
 
-public class MainActivity extends AppCompatActivity implements  DatePickerDialog.OnDateSetListener {
+public class MainActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
     private TextView tvDOB;
+    private TextView tvTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tvDOB = findViewById(R.id.tvDOB);
+        tvTime = findViewById(R.id.tvTime);
+
         tvDOB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 loadDatePicker();
+            }
+        });
+        tvTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loadTime();
             }
         });
     }
@@ -44,7 +54,27 @@ public class MainActivity extends AppCompatActivity implements  DatePickerDialog
 
     @Override
     public void onDateSet(DatePicker datePicker, int year, int month, int dayofMonth) {
-    String date = "Month Date Year:" + month +"/" +dayofMonth +"/"+year;
-    tvDOB.setText(date);
+        String date = "Month Date Year:" + month + "/" + dayofMonth + "/" + year;
+        tvDOB.setText(date);
+
     }
+
+    private void loadTime() {
+        final Calendar cal = Calendar.getInstance();
+        int hour = cal.get(Calendar.HOUR);
+        int minute = cal.get(Calendar.MINUTE);
+        final int second = cal.get(Calendar.SECOND);
+
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int hourofday, int minute) {
+                tvTime.setText("Time is" + hourofday + ":" + minute);
+
+            }
+        }, hour, minute, false);
+        timePickerDialog.show();
+    }
+
 }
+
+
